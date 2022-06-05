ad1cfb1a16750f5d010f215daf1dd6d5ea2ed9d9
Snoodie <45816295+Ozangeyam@users.noreply.github.com>
2022-06-06 06:24:20 +0800

    Update index.php

--- index.php
+++ index.php
@@ -312,18 +312,7 @@ require 'site_name.php';
                 <!-- <div class="btn-menu js-btn-menu"><span>&nbsp;</span><span>&nbsp;</span><span>&nbsp;</span></div> -->
             </div>
         </div>
-	 <div class="col-lg-5">
-                            <div id="google_translate_element"></div>
-
-
-                            <script type="text/javascript">
-                            function googleTranslateElementInit() {
-                              new google.translate.TranslateElement({pageLanguage: 'en'}, 'google_translate_element');
-                            }
-                            </script>
-
-                            <script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
-                        </div>
+	
     </header>    
 
     <!-- HEADER EOF   -->
