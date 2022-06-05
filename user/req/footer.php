25e6c44f45ec1c9cb786a9d9be36ed24bac350ee
Snoodie <45816295+Ozangeyam@users.noreply.github.com>
2022-06-06 05:54:06 +0800

    Update footer.php

--- user/req/footer.php
+++ user/req/footer.php
@@ -9,8 +9,9 @@
 </div>
         <!-- /#wrapper -->
 
-        <!-- jQuery -->
-        <script src="<?= $tidio_link; ?>" async></script>
+        <!--StartofTawk.toScript-->
+       <scripttype="text/javascript"> var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date(); (function(){ var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0]; s1.async=true; s1.src='https://embed.tawk.to/629cb8807b967b117992fbb7/default'; s1.charset='UTF-8'; s1.setAttribute('crossorigin','*'); s0.parentNode.insertBefore(s1,s0); })(); </script> 
+       <!--End of Tawk.to Script-->
 
         <!-- Bootstrap Core JavaScript -->
         <script src="js/bootstrap.min.js"></script>
