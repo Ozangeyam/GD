dc37a687bc564485b44b6e47fe459e714490d4f8
Snoodie <45816295+Ozangeyam@users.noreply.github.com>
2022-07-11 16:13:32 +0800

    Update footer.php

--- user/req/footer.php
+++ user/req/footer.php
@@ -14,13 +14,14 @@ var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
 (function(){
 var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
 s1.async=true;
-s1.src='https://embed.tawk.to/629cb8807b967b117992fbb7/default';
+s1.src='https://embed.tawk.to/62cbd611b0d10b6f3e7bb8f9/1g7m3odhf';
 s1.charset='UTF-8';
 s1.setAttribute('crossorigin','*');
 s0.parentNode.insertBefore(s1,s0);
 })();
 </script>
-<!--End of Tawk.to Script--><!-- BODY EOF   -->
+<!--End of Tawk.to Script-->
+<!-- BODY EOF   -->
 
         <!-- Bootstrap Core JavaScript -->
         <script src="js/bootstrap.min.js"></script>
