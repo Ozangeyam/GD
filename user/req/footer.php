0caa9f0a0d6c611af32452b7ad68ab7cdf708b7a
Snoodie <45816295+Ozangeyam@users.noreply.github.com>
2022-07-14 19:06:45 +0800

    Update footer.php

--- user/req/footer.php
+++ user/req/footer.php
@@ -20,7 +20,7 @@ d.getElementsByTagName("head")[0].appendChild(s);
 </script>
 <script  type="text/javascript">
   var config = {
-    phone :" 1 (469) 546-9681",
+    phone :" 1(469) 546-96-81",
     call :"Need Help?",
     position :"ww-right",
     size : "ww-normal",
