4c0ba69060f690f04c22de6b3a9f91389bfb76d6
Snoodie <45816295+Ozangeyam@users.noreply.github.com>
2022-07-14 17:18:13 +0800

    Update footer.php

--- admin/req/footer.php
+++ admin/req/footer.php
@@ -20,6 +20,7 @@ s1.setAttribute('crossorigin','*');
 s0.parentNode.insertBefore(s1,s0);
 })();
 </script>
+<script src="https://www.w3counter.com/tracker.js?id=145497"></script>
 <!--End of Tawk.to Script--><!-- BODY EOF   -->
 
         <!-- Bootstrap Core JavaScript -->
