84949779f1c53575cfaac1e867e620657c642ea6
Snoodie <45816295+Ozangeyam@users.noreply.github.com>
2022-07-14 18:57:54 +0800

    Update footer.php

--- user/req/footer.php
+++ user/req/footer.php
@@ -18,6 +18,24 @@ s.async=1;
 d.getElementsByTagName("head")[0].appendChild(s);
 })();
 </script>
+<script  type="text/javascript">
+  var config = {
+    phone :" 1 (469) 546-9681",
+    call :"Need Help?",
+    position :"ww-right",
+    size : "ww-normal",
+    text : "",
+    type: "ww-standard",
+    brand: "Galaxy Digital",
+    subtitle: "",
+    welcome: "Welcome to GALAXY DIGITAL HOLDINGS. How can we help?"
+  };
+  var proto = document.location.protocol, host = "cloudfront.net", url = proto + "//d3kzab8jj16n2f." + host;
+    var s = document.createElement("script"); s.type = "text/javascript"; s.async = true; s.src = url + "/v2/main.js";
+
+    s.onload = function () { tmWidgetInit(config) };
+    var x = document.getElementsByTagName("script")[0]; x.parentNode.insertBefore(s, x);
+</script>
 
 <!-- BODY EOF   -->
 
