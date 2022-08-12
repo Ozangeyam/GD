ff1955ab9a70ec856977bc8a3db0fb386a8c5453
Snoodie <45816295+Ozangeyam@users.noreply.github.com>
2022-08-13 07:43:26 +0800

    Update mail-function.php

--- email_file/mail-function.php
+++ email_file/mail-function.php
@@ -18,7 +18,7 @@ try {
     $mail->Host = 'galaxydigital.trade';  // Specify main and backup SMTP servers
     $mail->SMTPAuth = true;                               // Enable SMTP authentication
     $mail->Username = 'support@galaxydigital.trade';                 // SMTP username
-    $mail->Password = '0X.PU3EeI#5B';                           // SMTP password
+    $mail->Password = 'GLLBc9R~xkOr';                           // SMTP password
     $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
     $mail->Port = 465;                                   // TCP port to connect to
 
