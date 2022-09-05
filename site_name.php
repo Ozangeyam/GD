11c1b54db3ac88128bc13c58765cb172e2a568ea
Snoodie <45816295+Ozangeyam@users.noreply.github.com>
2022-09-06 05:41:05 +0800

    Update site_name.php

--- site_name.php
+++ site_name.php
@@ -6,5 +6,5 @@ $support_email = "support@galaxydigitals.trade";
 $company_address = "300 Vesey Street, New York, NY 10282 United States";
 $site_logo = $site_url . "/img/up_logo.png";
 $site_logo2 = $site_url . "/img/up_logo2.png";
-$cert = $site_url . "/en/img/cert.png" "https://www.uid.admin.ch/Detail.aspx?uid_id=CHE-106.061.288&lang=en";
+$cert = $site_url . "/en/img/cert.png";
 $favicon = $site_url . "/img/favicon.ico";
